4b4e90bd9985c9c23cfe414e0239f1554f85d10d
darissultan <sultan.daris.nuryanto-2022@fst.unair.ac.id>
2026-04-25 23:09:13 +0700

    continuing the vector section and learning more about pointer in struct context

---
+++ week2/vector.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct vector vector;
+
+struct vector
+{
+    int* current;
+    int* min;
+    int* max;
+    int size; 
+};
+
+void vpop(struct vector *temp){
+    if((char *)temp->max - (char *)temp->current == 1){
+        char* val = (char*)(&temp->current); 
+        (*val)-=3;
+    }else if(temp->current == temp->min){
+        printf("tidak ada yang bisa di pop, memori sudah kosong!\n");
+    }else{
+        (temp->current)--;
+        printf("3\n");
+    }
+
+    char* target = (char*)temp->current; 
+    int i;
+    for (i = 0; i < 4; i++) {
+        *target = 0; 
+        target++;
+    }
+}
+
+void vpush(struct vector *temp, int val){
+    if(((temp->current)+1) > temp->max){
+        printf("tidak bisa push lagi, memori akan di resize\n");
+        vresize(&temp->current);
+        vpush(&temp, val);
+        return;
+    }
+
+    char* target = (char*)temp->current; 
+    char* nilai = (char*)&val;
+    int i = 0;
+    for (i = 0; i < 4; i++) {
+        *target = *nilai; 
+        target++;
+        nilai++;
+    }
+
+    if(((temp->current)+1) == temp->max){
+        char* val = (char*)(&temp->current); 
+        (*val)+=3;
+    }else{
+        ((temp->current))++;
+    }
+};
+
+void vresize(vector *x){
+    int offset = (char *)x->current - (char *)x->min;
+    int *baru = realloc(x->min, 2*(x->size) * sizeof(int));  
+    
+    printf("%d %d %d--- \n", x->current, x->max, x->min);
+
+    x->size *= 2;
+    x->current = x->min;
+    x->min = baru;
+    x->max = x->min + x->size;
+    
+    printf("%d %d %d--- \n", x->current, x->max, x->min);
+}
+
+void show(struct vector val){
+    int* i;
+    for(i = val.min; i < val.max; i++){
+        printf("%d ",*i);
+    };        
+    printf("\n");
+}
+
+int main(){
+    vector v;
+
+    /*initializing*/
+    v.min = malloc(2 * sizeof(int));
+    v.current = v.min;
+    v.max = v.min + 2;
+    v.size = 2;
+
+    vpush(&v,10);
+    vpush(&v,20);
+    show(v);
+
+    vpush(&v,30);
+    show(v);
+
+    return 0;
+}
